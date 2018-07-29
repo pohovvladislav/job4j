75a35f1633cd3e89fde3bb5614849af84d9405f0
popovvladislav <navodbmd.2@mail.ru>
2018-07-29 17:43:15 +0300

    1.3.JavaDoc. Code convention[#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,4 +1,8 @@
 package ru.job4j.calculate;
+/**
+* Calculate
+* @author Popov Vladislav (navodbmd.2@mail.ru)
+*/
 
 public class Calculate{
 
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Vladislav Popov (mailto:navodbmd.2@mail.ru)
+* @version  $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
