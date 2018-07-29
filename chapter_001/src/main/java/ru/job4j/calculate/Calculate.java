d54fdcbe2bd34a0bbf5288adad2f6dfab5a1fd2e
popovvladislav <navodbmd.2@mail.ru>
2018-07-29 20:55:56 +0300

    Remove target

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -9,5 +9,14 @@ public class Calculate{
 	public static void main(String[]args){
 	
 	System.out.println("Hello World");
-}
+	}
+/**
+* Method echo.
+* @param name 
+* @return Echo plus name.
+*/
+
+public String echo(String name){
+	return "Echo, echo, echo:" + name;
+	}
 }
