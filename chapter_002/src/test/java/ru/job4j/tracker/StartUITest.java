56e16b53e4eaa57336907621d34e963837c1730c
popovvladislav <navodbmd.2@mail.ru>
2018-11-02 21:36:51 +0300

    4.6. Доработать тесты в StartUITest проверяющие вывод данных в консоль. [#33585]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,13 +1,31 @@
 package ru.job4j.tracker;
 /**
  *
+ * @version $Id$
+ * @since 0.1
  */
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 public class StartUITest {
+    private final PrintStream stdout = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.setOut(new PrintStream(this.out));
+    }
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+    }
+
     @Test
     public void whenUserAddItemThenTrackerHasNewItemWithSameName() {
         Tracker tracker =  new Tracker();
@@ -31,5 +49,29 @@ public class StartUITest {
         Input input = new StubInput(new String[] {"3", first.getId(), "6"});
         new StartUI(input, tracker).init();
         assertThat(tracker.findAll()[0], is(second));
+    }
+    @Test
+    public void whenFindAll() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("name", "desc", System.currentTimeMillis()));
+        Input input = new StubInput(new String[] {"1", "6"});
+        new StartUI(input, tracker).init();
+        assertThat(tracker.findById(item.getId()).getName(), is("name"));
+    }
+    @Test
+    public void whenFindByName() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("name", "desc", System.currentTimeMillis()));
+        Input input = new StubInput(new String[] {"5", "name", "6"});
+        new StartUI(input, tracker).init();
+        assertThat(tracker.findById(item.getId()).getName(), is("name"));
+    }
+    @Test
+    public void whenFindById() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("name", "desc", System.currentTimeMillis()));
+        Input input = new StubInput(new String[] {"4", item.getId(), "6"});
+        new StartUI(input, tracker).init();
+        assertThat(tracker.findAll()[0].getName(), is("name"));
     }
 }
