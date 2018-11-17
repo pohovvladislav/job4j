787cdae6601119dfa9d4c10d87e00b7d7713a8f6
popovvladislav <navodbmd.2@mail.ru>
2018-11-17 22:48:13 +0300

    6.3. if-else-throw [#49895]

--- chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
@@ -3,27 +3,41 @@ package ru.job4j.tracker;
  * A class for entering user data from the console.
  */
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.function.Consumer;
 
 public class ConsoleInput implements Input {
     private Scanner scanner = new Scanner(System.in);
+    boolean exist = false;
+  /**  private final List<Consumer<Input>> validates = Arrays.asList(
+            input -> {
+                if (!exist) {
+                    throw new MenuOutException("Выход за пределы списка!");
+                }
+            }
+    );
+*/
+
     public String ask(String question) {
         System.out.println(question);
         return scanner.nextLine();
     }
     public int ask(String question, List<Integer> range) {
+
         int key = Integer.valueOf(this.ask(question));
-        boolean exist = false;
+
         for (int value : range) {
             if (value == key) {
                 exist = true;
             }
         }
-        if (exist) {
-            return key;
-        } else {
+
+       // this.validates.forEach(action -> action.accept(???));
+        if (!exist) {
             throw new MenuOutException("Выход за пределы списка!");
         }
+        return key;
     }
 }
