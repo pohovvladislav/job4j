33ef2bb5e99ebc2894b64ac1648cb5a34aad620b
popovvladislav <navodbmd.2@mail.ru>
2018-12-16 12:01:14 +0300

    2. Сортировка User с использованием Comparator [#10036]

--- chapter_002/src/main/java/ru/job4j/list/UserConvert.java
+++ chapter_002/src/main/java/ru/job4j/list/UserConvert.java
@@ -1,13 +1,12 @@
 package ru.job4j.list;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.TreeSet;
+import java.util.*;
 
 /**
  * @author В-87.
  */
 public class UserConvert {
+
     /**
      * The method converts List to Map.
      * @param list ArrayList<User>.
@@ -22,15 +21,48 @@ public class UserConvert {
     }
 
     /**
-     * The method sorts users by age/
+     * The method sorts users by age.
      * @param list List of Users.
      * @return TreeSet Users.
      */
     public TreeSet<User> sort(List<User> list) {
-        TreeSet<User> set = new TreeSet<>();
-        for (User user : list) {
-            set.add(user);
-        }
-        return set;
+        return new TreeSet<>(list);
+    }
+
+    /**
+     * The method sorts users by name length.
+     * @param list List of Users.
+     * @return Sort List.
+     */
+    public List<User> sortNameLength(List<User> list) {
+        List<User> nameLength = new ArrayList<>(list);
+        nameLength.sort(
+                new Comparator<User>() {
+                    @Override
+                    public int compare(User o1, User o2) {
+                        return Integer.compare(o1.getName().length(), o2.getName().length());
+                    }
+                }
+        );
+        return nameLength;
+    }
+
+    /**
+     * The method sorts users by all fields.
+     * @param list List of Users.
+     * @return Sort List.
+     */
+    public List<User> sortByAllFields(List<User> list) {
+        List<User> allFields = new ArrayList<>(list);
+        allFields.sort(
+                new Comparator<User>() {
+                    @Override
+                    public int compare(User o1, User o2) {
+                        int rsl = o1.getName().compareTo(o2.getName());
+                        return rsl != 0 ? rsl : Integer.compare(o1.getAge(), o2.getAge());
+                    }
+                }
+        );
+        return allFields;
     }
 }
--- chapter_002/src/test/java/ru/job4j/list/UserConvertTest.java
+++ chapter_002/src/test/java/ru/job4j/list/UserConvertTest.java
@@ -2,10 +2,7 @@ package ru.job4j.list;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.TreeSet;
+import java.util.*;
 
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
@@ -13,22 +10,34 @@ import static org.hamcrest.core.Is.is;
 /**
  * The test of  program UserConvert.
  */
+
 public class UserConvertTest {
     UserConvert convert = new UserConvert();
-    ArrayList<User> list = new ArrayList<>();
+    ArrayList<User> list = new ArrayList<>(Arrays.asList(
+            new User(1, "Nikolai", 30, "Kuibyshev"),
+            new User(4, "Valera", 32, "Leningrad"),
+            new User(7, "Egor", 27, "Vladivostok"),
+            new User(11, "Nikolai", 29, "Minsk"))
+    );
     @Test
     public void whenUsersOfArrayListThenUsersOfHashMap() {
-        list.add(new User(1, "Ivan", 30,"Kuibyshev"));
         HashMap<Integer, User> result = convert.process(list);
         assertTrue(result.containsKey(1));
     }
     @Test
     public void whenUsersOfArrayListThenTreeSetUsersSortOfAge() {
-        list.addAll(Arrays.asList(
-                new User(1, "Ivan", 30,"Kuibyshev"),
-                new User(4, "Valera", 32,"Leningrad"),
-                new User(7, "Egor", 27,"Vladivostok")));
         TreeSet<User> result = convert.sort(list);
         assertThat(result.first().getName(), is("Egor"));
+    }
+    @Test
+    public void whenAddUsersByArrayListThanSortByNameLength() {
+        List<User> result = convert.sortNameLength(list);
+        assertThat(result.get(0).getName(), is("Egor"));
+    }
+    @Test
+    public void whenAddUsersByArrayListThanSortByAllFields() {
+        //list.add(new User(11, "Nikolai", 17,"Kuibyshev"));
+        List<User> result = convert.sortByAllFields(list);
+        assertThat(result.get(2).getName() + result.get(2).getAge(), is("Nikolai" + 30));
     }
 }
