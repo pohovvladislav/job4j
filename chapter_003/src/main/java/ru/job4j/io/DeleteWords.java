f0ede1395422a6eb7f71eea66516366dc32e0406
popovvladislav <navodbmd.2@mail.ru>
2019-03-03 22:25:33 +0300

    2. Удаление запрещенных слов [#859]

--- chapter_003/src/main/java/ru/job4j/io/DeleteWords.java
+++ chapter_003/src/main/java/ru/job4j/io/DeleteWords.java
@@ -1,35 +1,60 @@
 package ru.job4j.io;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.StringReader;
 import java.util.Arrays;
 
 /**
- *
+ * @author В-87
  */
 public class DeleteWords {
-
+    /**
+     * The method removes words from the array that are part of the in stream and not part of the out stream.
+     * @param in InputStream.
+     * @param out OutputStream.
+     * @param abuse Array.
+     */
     public void dropAbuses(InputStream in, OutputStream out, String[] abuse) {
 
-        try (InputStreamReader input = new InputStreamReader(in);
-             OutputStreamWriter output = new OutputStreamWriter(out)) {
-
-            StringBuilder builder = new StringBuilder();
+        try (InputStream input = in;
+             OutputStream output = out) {
+            StringReader readerOut = new StringReader(output.toString());
+            StringBuilder builderIn = new StringBuilder();
+            StringBuilder builderOut = new StringBuilder();
 
             int var;
-            while ((var =  input.read()) != -1) {
+            while ((var = input.read()) != -1) {
+                builderIn.append((char) var);
+                if (var == 32) {
 
-                System.out.println((char) var);
-
-
+                    int outVar;
+                    if (builderOut.length() == 0) {
+                        while ((outVar = readerOut.read()) != -1) {
+                            builderOut.append((char) outVar);
+                            if (outVar == 32) {
+                                break;
+                            }
+                        }
+                    }
+                    if (builderIn.toString().equals(builderOut.toString())) {
+                        builderIn.setLength(0);
+                        builderOut.setLength(0);
+                    } else {
+                        String word = builderIn.deleteCharAt(builderIn.length() - 1).toString();
+                        for (int i = 0; i < abuse.length; i++) {
+                            if (abuse[i].equals(word)) {
+                                System.arraycopy(abuse, i + 1, abuse, i, abuse.length - 1 - i);
+                                abuse = Arrays.copyOf(abuse, abuse.length - 1);
+                                builderIn.setLength(0);
+                            }
+                        }
+                    }
+                }
             }
-
-
-            System.out.println(output.toString());
-
-
         } catch (IOException e) {
             e.printStackTrace();
-
         }
     }
 }
--- chapter_003/src/test/java/ru/job4j/io/DeleteWordsTest.java
+++ chapter_003/src/test/java/ru/job4j/io/DeleteWordsTest.java
@@ -4,24 +4,23 @@ import org.junit.Test;
 
 import java.io.*;
 
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class DeleteWordsTest {
     @Test
-    public void when() throws IOException {
+    public void test() throws IOException {
         DeleteWords deleteWords = new DeleteWords();
         String text = "hello ass have are you";
         String text1 = "hello have are you";
         String[] abuse = {"name", "test", "ass"};
 
-        byte[] array = text.getBytes();
-        byte[] a = text1.getBytes();
-        InputStream stream = new ByteArrayInputStream(array);
-        OutputStream outputStream = new ByteArrayOutputStream(a.length);
-        outputStream.write(a);
+        byte[] input = text.getBytes();
+        byte[] output = text1.getBytes();
+
+        InputStream stream = new ByteArrayInputStream(input);
+        OutputStream outputStream = new ByteArrayOutputStream(output.length);
+        outputStream.write(output);
         deleteWords.dropAbuses(stream, outputStream, abuse);
-
-
     }
-
 }
