335899e18fcdacd6f04bf415dadb7e259212e890
popovvladislav <navodbmd.2@mail.ru>
2019-03-16 12:36:41 +0300

    3. Сканирование файловой системы. [#106929]

---
+++ chapter_003/src/main/java/ru/job4j/io/Search.java
@@ -0,0 +1,44 @@
+package ru.job4j.io;
+
+import java.io.File;
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * @author В-87
+ */
+public class Search {
+    /**
+     * The method returns a list of all files with a specific extension.
+     *
+     * @param parent The  path to the directory from which you want to search.
+     * @param exts   File extensions we want to get.
+     * @return A list of all files with a specific extension.
+     */
+    public List<File> files(String parent, List<String> exts) {
+        List<File> list = new ArrayList<>();
+        Queue<File> queue = new LinkedList<>();
+        queue.add(new File(parent));
+        while (!queue.isEmpty()) {
+            File poll = queue.poll();
+
+            if (poll.isDirectory()) {
+                queue.addAll(Arrays.stream(poll.listFiles()).collect(Collectors.toList()));
+            } else {
+
+                String fileName = poll.getName();
+                int pos = fileName.lastIndexOf(".");
+                if (pos > 0) {
+                    fileName = fileName.substring(pos);
+                }
+
+                for (String str : exts) {
+                    if (str.equals(fileName)) {
+                        list.add(poll);
+                    }
+                }
+            }
+        }
+        return list;
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/io/SearchTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class SearchTest {
+
+    @Test
+    public void searchTest() {
+        Search search = new Search();
+        List<String> list = Arrays.asList(".html", ".css");
+        List<String> expect = Arrays.asList(
+                "C:\\projects\\job4j\\chapter_003\\src\\main\\java\\ru\\job4j\\io\\tmpdir\\file.html",
+                "C:\\projects\\job4j\\chapter_003\\src\\main\\java\\ru\\job4j\\io\\tmpdir\\dir\\file.css",
+                "C:\\projects\\job4j\\chapter_003\\src\\main\\java\\ru\\job4j\\io\\tmpdir\\dir\\file_2.css"
+        );
+        assertThat(search.files(System.getProperty("user.dir"), list).stream().map(file -> file.getAbsolutePath())
+                .collect(Collectors.toList()), is(expect)
+        );
+    }
+}
